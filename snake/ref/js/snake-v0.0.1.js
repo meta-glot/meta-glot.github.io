840213c97de5cc7a554ec18d50051e9f39879a97
meta-glot <103900127+meta-glot@users.noreply.github.com>
2022-05-20 09:26:04 -0400

    Update snake-v0.0.1.js
    Reverted to retain utility

--- snake/ref/js/snake-v0.0.1.js
+++ snake/ref/js/snake-v0.0.1.js
@@ -1,104 +1,109 @@
 $(function(){
-    let svg = Pablo('#ground').svg({ //create svg with height and width
-                  width: 1100,
-                  height: 450
-              });
 
-    let arrayNode = [];
-    
-    let nodeSize = 15;
-    let direction = 1;
-    let speed = 200;
-    let arrayColor = new Array("#ef5777","#ffc048","#575fcf","#05c46b","#8e44ad");
-    let colorIndex = 0;
+var svg = Pablo('#ground').svg({ //create svg with height and width
+         width: 1100,
+        height: 700
+      });
 
-    snakeBuilder(4,100,100);
-    console.log(arrayNode);
+var arrayNode=new Array();
+var nodeSize=15;
 
-    setInterval(function(){
-        snakeMove();
-    }, speed);
+var direction=1;
 
-    $(document).keydown(function(event){
-        //37 left  - 38  up - 39  right - 40  down
-        let code =  event.which;
-        
-        if ( direction == 1 || direction == 3 ) {
-            if ( code == 38 ) direction = 4;
-            if ( code == 40 ) direction = 2;
-        }
+var speed=200;
 
-        if ( direction == 2 || direction == 4 ) {
-            if ( code == 39 ) direction = 1;
-            if ( code == 37 ) direction = 3;
-        }
-    });
 
-    function snakeMove() {
-        // access to the x property of the last rectangle of the array
-        let x = arrayNode[arrayNode.length-1].attr('x');
-        // access to the y property of the last rectangle of the array
-        let y = arrayNode[arrayNode.length-1].attr('y');
+var arrayColor=new Array("#ef5777","#ffc048","#575fcf","#05c46b","#8e44ad");
+var colorIndex=0;
 
-        // if we are moving right, move on the +X axis
-        if ( direction == 1 ) {
-            x = parseInt(x) + nodeSize;
-        }
+snakeBuilder(4,100,100);
+console.log(arrayNode);
 
-        // if we are moving down, move on the +Y axis
-        if ( direction == 2 ) {
-            y = parseInt(y) + nodeSize;
-        }
+setInterval(function(){
+  snakeMove();
+}, speed);
 
-        // if we are moving left, move on the -X axis
-        if ( direction == 3 ) {
-            x = parseInt(x) - nodeSize;
-        }
-             
-        // if we are moving up, move on the -Y axis
-        if ( direction == 4 ) {
-            y = parseInt(y) - nodeSize;
-        }             
-             
-        let snakeNode = buildSnakeNode (svg, x, y );
 
-        // remove first svg element
-        arrayNode[0].remove();
-             
-        //delete first element in array
-        arrayNode.shift();
+$(document).keydown(function(event){
+  //37 left  - 38  up - 39  right - 40  down
 
-        // add the last element
-        arrayNode.push(snakeNode);
-    }
+     var code =  event.which;
 
-    // initialize snake
-    function snakeBuilder (node, x, y) {
-        for (let i = 0; i < node; i++) {
+     if(direction==1||direction==3){
+       if(code==38) direction=4;
+       if(code==40) direction=2;
+     }
 
-            // build next snake body part
-            let snakeNode = buildSnakeNode (svg, x, y );
-            
-            // increment x position size of snake body part
-            x += nodeSize;
+     if(direction==2||direction==4){
+       if(code==39) direction=1;
+       if(code==37) direction=3;
+     }
 
-            // add next part to snake
-            arrayNode.push(snakeNode);//add node
-        }
-    }
-         
-    let buildSnakeNode = ( svg, x, y ) => {
-        let fill = arrayColor[Math.floor(Math.random() * arrayColor.length)];
-        let snakeNode = svg.rect({
-                              x,
-                              y,
-                              width:nodeSize,
-                              height:nodeSize,
-                              fill,
-                              stroke:'#006',
-                              'stroke-width': 2,
-                              'stroke-linejoin': 'round'
-        });
-        return snakeNode;
-    }
+
+});
+
+
+function snakeMove(){
+  var x;
+  var y;
+
+  x=arrayNode[arrayNode.length-1].attr('x');//access to the x property of the last rectangle of the array
+  y=arrayNode[arrayNode.length-1].attr('y');//access to the y property of the last rectangle of the array
+
+if(direction==1){
+   x=parseInt(x)+nodeSize;//Move on the + X axis
+}
+
+if(direction==2){
+   y=parseInt(y)+nodeSize;//Move on the +Y axis
+}
+
+if(direction==3){
+   x=parseInt(x)-nodeSize;//Move on the - X axis
+}
+if(direction==4){
+   y=parseInt(y)-nodeSize;//Move on the - Y axis
+}
+
+  var colorIndex=Math.floor(Math.random() * 5);
+
+  var snakeNode=svg.rect({
+      x:x, y:y,
+      width:nodeSize, height:nodeSize,
+      fill:  arrayColor[colorIndex],
+      stroke:'#006',
+      'stroke-width': 2,
+      'stroke-linejoin': 'round'
+  });
+
+
+  arrayNode[0].remove();//remove first svg element
+  arrayNode.shift();//delete first element in array
+
+  arrayNode.push(snakeNode);//add last element
+
+}
+
+//create snake
+function snakeBuilder(node,x,y){
+
+  for (var i = 0; i < node; i++) {
+
+  var colorIndex=Math.floor(Math.random() * 5);//A random number between 0 and 5
+
+      var snakeNode=svg.rect({
+          x:x, y:y,
+          width:nodeSize, height:nodeSize,
+          fill:arrayColor[colorIndex],
+          stroke:'#006',
+          'stroke-width': 2,
+          'stroke-linejoin': 'round'
+      });
+      x=x+nodeSize;
+
+      arrayNode.push(snakeNode);//add node
+  }
+
+}
+
 });
