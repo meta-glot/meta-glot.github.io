bcea9974abd2452d19107cb244babbaf0b9a0f12
meta-glot <103900127+meta-glot@users.noreply.github.com>
2022-05-20 14:11:15 -0400

    Update and rename snake-v0.0.1.js to snake-v0.0.2.js
    Refactor - renamed, did some script cleanup

--- snake/ref/js/snake-v0.0.2.js
+++ snake/ref/js/snake-v0.0.2.js
@@ -1,21 +1,17 @@
 $(function(){
 
-var svg = Pablo('#ground').svg({ //create svg with height and width
+let svg = Pablo('#ground').svg({ //create svg with height and width
          width: 1100,
-        height: 700
+        height: 400
       });
 
-var arrayNode=new Array();
-var nodeSize=15;
-
-var direction=1;
-
-var speed=200;
-
-
-var arrayColor=new Array("#ef5777","#ffc048","#575fcf","#05c46b","#8e44ad");
-var colorIndex=0;
-
+let arrayNode = [],
+    nodeSize = 15,
+    direction = 1,
+    speed = 200,
+    arrayColor = new Array("#ef5777","#ffc048","#575fcf","#05c46b","#8e44ad"),
+    colorIndex = 0;
+       
 snakeBuilder(4,100,100);
 console.log(arrayNode);
 
@@ -23,11 +19,10 @@ setInterval(function(){
   snakeMove();
 }, speed);
 
-
 $(document).keydown(function(event){
   //37 left  - 38  up - 39  right - 40  down
 
-     var code =  event.which;
+     let code =  event.which;
 
      if(direction==1||direction==3){
        if(code==38) direction=4;
@@ -44,11 +39,8 @@ $(document).keydown(function(event){
 
 
 function snakeMove(){
-  var x;
-  var y;
-
-  x=arrayNode[arrayNode.length-1].attr('x');//access to the x property of the last rectangle of the array
-  y=arrayNode[arrayNode.length-1].attr('y');//access to the y property of the last rectangle of the array
+  let x = arrayNode[arrayNode.length-1].attr('x');//access to the x property of the last rectangle of the array
+  let y = arrayNode[arrayNode.length-1].attr('y');//access to the y property of the last rectangle of the array
 
 if(direction==1){
    x=parseInt(x)+nodeSize;//Move on the + X axis
@@ -65,9 +57,9 @@ if(direction==4){
    y=parseInt(y)-nodeSize;//Move on the - Y axis
 }
 
-  var colorIndex=Math.floor(Math.random() * 5);
+  let colorIndex=Math.floor(Math.random() * 5);
 
-  var snakeNode=svg.rect({
+  let snakeNode=svg.rect({
       x:x, y:y,
       width:nodeSize, height:nodeSize,
       fill:  arrayColor[colorIndex],
@@ -79,7 +71,6 @@ if(direction==4){
 
   arrayNode[0].remove();//remove first svg element
   arrayNode.shift();//delete first element in array
-
   arrayNode.push(snakeNode);//add last element
 
 }
@@ -87,11 +78,11 @@ if(direction==4){
 //create snake
 function snakeBuilder(node,x,y){
 
-  for (var i = 0; i < node; i++) {
+  for (let i = 0; i < node; i++) {
 
-  var colorIndex=Math.floor(Math.random() * 5);//A random number between 0 and 5
+  let colorIndex=Math.floor(Math.random() * 5);//A random number between 0 and 5
 
-      var snakeNode=svg.rect({
+      let snakeNode=svg.rect({
           x:x, y:y,
           width:nodeSize, height:nodeSize,
           fill:arrayColor[colorIndex],
